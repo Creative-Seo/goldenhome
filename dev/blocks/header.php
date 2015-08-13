d5823ddfdf8a5ee8a13bf3d320a7fe615d571e3e
creativeseo33 <alexeyfedotof@gmail.com>
2015-08-13 16:18:31 +0300

    logo

--- dev/blocks/header.php
+++ dev/blocks/header.php
@@ -6,7 +6,7 @@
 		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
 		<div class="header-logo">
 		<a href="/">
-<?php if (HTTP_ID == '/') { ?><img class="img-responsive" src="/img/logo.png" alt="Агентство права и оценки" />
+<?php if (HTTP_ID == '/') { ?><img class="img-responsive" src="/img/logo.png" alt="Агентство права и оценки" width="283">
 <?php } else { ?><img class="img-responsive" src="/img/logo-black.png" alt="Агентство права и оценки" />
 <?php } ?>
 		</a>
