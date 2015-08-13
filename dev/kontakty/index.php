664f1acb0de65fa38c0917c664321ba368e63965
CreativeSeo33 <alexeyfedotof@gmail.com>
2015-08-13 23:55:30 +0300

    contacts

--- dev/kontakty/index.php
+++ dev/kontakty/index.php
@@ -8,7 +8,8 @@ $breadcrumb = 'Проекты домов';
 include($root."blocks/meta.php"); 
 include($root."blocks/header.php"); 
 ?>
-<div class="container contacts">
+<div class="contacts">
+<div class="container">
   <div class="row">
     <h1>Контактная информация</h1>
   </div>
@@ -20,13 +21,93 @@ include($root."blocks/header.php");
       </ul>
     </div>    
   </div>
-  <div class="row">
-    <div class="tab-content">
-      <div role="tabpanel" class="tab-pane" id="vladimir">asdasdsad</div>
-      <div role="tabpanel" class="tab-pane" id="ivanovo">asdsasd</div>
-    </div>
-  </div>
+  
 </div>
+<div class="container-fluid">
+<div class="row">
+    <div class="tab-content">
+      <div role="tabpanel" class="tab-pane fade" id="vladimir">
+
+       <div class="contacts_phone">
+        <div><i class="fa fa-phone"></i>+7 (4922) 37-73-90</div>
+        <div class="contacts_mail"><i class="fa fa-paper-plane"></i><a href="mailto:goldenhome33@mail.ru">goldenhome33@mail.ru</a></div>
+        <div class="contacts_phone_descr">по вопросам строительства и продажи домов</div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div><i class="fa fa-phone"></i>+7 (4922) 37-73-92</div>
+        <div class="contacts_phone_descr">по проектам домов</div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div><i class="fa fa-phone"></i>+7 (4922) 37-73-91</div>
+        <div class="contacts_mail"><i class="fa fa-paper-plane"></i><a href="mailto:design@gh33.ru">design@gh33.ru</a></div>
+        <div class="contacts_phone_descr">по дизайну интерьера</div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div><i class="fa fa-hourglass-o"></i>Время работы: </div>
+        <div class="contacts_phone_descr">пн-пт 09:00-18:00, сб.вс. - выходной</div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div><i class="fa fa-map-marker"></i>Адрес: </div>
+        <div class="contacts_phone_descr">600017, г. Bлaдимиp, ул. Мира, д. 15 В, офис 12, 3 этаж (Деловой центр «Консоль»)</div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_map">
+         <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=9UmBXK82LmOHB7PR7120e5LGdSUMR-8Y&width=auto&height=450"></script>
+       </div>
+
+      </div>
+      <div role="tabpanel" class="tab-pane fade" id="ivanovo">
+       <div class="contacts_phone">
+        <div><i class="fa fa-phone"></i>+7 (4932) 92-90-99</div>
+
+        <div class="line center-block"></div>
+
+        <div class="contacts_phone">
+          <div class="contacts_mail"><i class="fa fa-paper-plane"></i><a href="mailto:info@sk-goldenhome.ru">info@sk-goldenhome.ru</a></div>
+        </div>
+
+        <div class="line center-block"></div>
+
+        <div class="contacts_phone">
+          <div><i class="fa fa-hourglass-o"></i>Время работы: </div>
+          <div class="contacts_phone_descr">пн-пт 09:00-18:00, сб.вс. - выходной</div>
+        </div>
+
+        <div class="line center-block"></div>
+
+        <div class="contacts_phone">
+          <div><i class="fa fa-map-marker"></i>Адрес: </div>
+          <div class="contacts_phone_descr">153009, г. Иваново, ул. Лежневская,д.183, оф. 407 (ТД «Славянский»)</div>
+        </div>
+
+        <div class="line center-block"></div>
+
+        <div class="contacts_map">
+          <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=pS5Ida2yCLSPdFcapOs9iOBHXqvr9yZb&width=auto&height=450"></script>
+        </div>
+
+
+       </div>
+      </div>
+    </div>
+</div>
+</div>
+
+</div> <!-- contacts -->
 
 <?php include($root."blocks/footer.php");?>
 <script type="text/javascript">
