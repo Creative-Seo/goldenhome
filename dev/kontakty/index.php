74a5a7c9239b30a56b73671a246b55b410599c16
CreativeSeo33 <alexeyfedotof@gmail.com>
2015-08-14 01:32:05 +0300

    new

--- dev/kontakty/index.php
+++ dev/kontakty/index.php
@@ -26,6 +26,7 @@ include($root."blocks/header.php");
 <div class="container-fluid">
 <div class="row">
     <div class="tab-content">
+      <!-- Владимир -->
       <div role="tabpanel" class="tab-pane fade" id="vladimir">
 
        <div class="contacts_phone">
@@ -68,8 +69,30 @@ include($root."blocks/header.php");
        <div class="contacts_map">
          <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=9UmBXK82LmOHB7PR7120e5LGdSUMR-8Y&width=auto&height=450"></script>
        </div>
+        
+        <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div></i>Дополнительные контакты:</div>
+        <div class="contacts_mail">Skype : <a href="callto://GoldenHome33">GoldenHome33</a></div>
+        <div class="contacts_mail"><i class="fa fa-paper-plane"></i><a href="mailto:info@sk-goldenhome.ru">info@sk-goldenhome.ru</a></div>
+       </div>
+
+       <div class="line center-block"></div>
+
+       <div class="contacts_phone">
+        <div>Реквизиты</div>
+        <div class="contacts_phone_descr">Общество с ограниченной ответственностью «Голден Хоум»</div>
+        <div class="contacts_phone_descr">ОГРН: 1143328003532</div>
+        <div class="contacts_phone_descr">ИНН: 3328497491</div>
+        <div class="contacts_phone_descr">КПП: 332801001</div>
+        <div class="contacts_phone_descr">Юридический адрес: 600017, г. Владимир, ул. Мира, д. 15, 2</div>
+       </div>
 
       </div>
+      <!-- Владимир -->
+
+      <!-- Иваново -->
       <div role="tabpanel" class="tab-pane fade" id="ivanovo">
        <div class="contacts_phone">
         <div><i class="fa fa-phone"></i>+7 (4932) 92-90-99</div>
@@ -100,9 +123,36 @@ include($root."blocks/header.php");
           <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=pS5Ida2yCLSPdFcapOs9iOBHXqvr9yZb&width=auto&height=450"></script>
         </div>
 
-
+        <div class="line center-block"></div>
+        
        </div>
       </div>
+      <!-- Иваново -->
+      
+
+      <!-- Форма обратной связи -->
+
+<div class="callback-form floating-labels center-block cd-form">
+    <h2>Форма обратной связи</h2>
+  
+  <div class="callback-form_note"> Оставьте сообщение и контактные данные. Наши сотрудники свяжутся с вами в рабочее время и ответят на все возникшие вопросы. </div>
+  <div class="form-group icon">
+    <label class="cd-label" for="cd-name">Имя</label>
+    <input id="cd-name" class="user callback-form_phone form-control" type="text" required="" name="cd-name">
+  </div>
+  <div class="form-group icon">
+    <label class="cd-label" for="cd-phone">Телефон</label>
+    <input id="cd-phone" class="user callback-form_phone form-control" type="text" required="" name="cd-phone">
+  </div>
+  <div class="form-group icon">
+    <label class="cd-label" for="cd-textarea">Сообщение</label>
+    <textarea id="cd-textarea" class="message" required="" name="cd-textarea"></textarea>
+  </div>
+  <button class="btn btn-default callback-form_button">Отправить</button>
+</div>
+
+<!-- Форма обратной связи -->
+
     </div>
 </div>
 </div>
--- dev/kupit-dom/index.php
+++ dev/kupit-dom/index.php
@@ -7,64 +7,68 @@ $breadcrumb = 'Купить дом';
 include($root."blocks/meta.php"); 
 include($root."blocks/header.php"); 
 ?>
-<div class="container">			
-<?php include($root."blocks/breadcrumbs.php");?>                
-<h1 class="hdom">Дома и коттеджи во Владимирской области</h1>
+<div class="container">		
+
+             
+<h1>Дома и коттеджи во Владимирской области</h1>
    
-   <div id="map"></div> 
 
-<!-- item -->
 <div class="row">
-<div class="col-lg-4 col-md-4 col-sm-4">
-<div class="thumbnail">
-<a href="v-novom-sele/"><img class="media-object img-responsive" alt="Дом (коттедж), село Новое" src="v-novom-sele/novoe.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 25</div>
-<div class="caption">
-<h4><a href="v-novom-sele/">Дом (коттедж), село Новое</a></h4>
-<ul class="list-unstyled list-inline search-meters"><li>
-<span class="stoit"><b>4 650 000</b> <i class="fa fa-rub"></i></span>
-</li>
-<li>181 м<sup>2</sup></li>
-<li>8,6 соток</li>
-</ul><p class="shosse tck">Суздальский район, село Новое</p>
-</div>
-</div>
-</div>
-<!-- /item -->
+	<!-- item -->
+	<div class="row">
+	<div class="col-lg-4 col-md-4 col-sm-4">
+	<div class="thumbnail">
+	<a href="v-novom-sele/"><img class="media-object img-responsive" alt="Дом (коттедж), село Новое" src="v-novom-sele/novoe.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 25</div>
+	<div class="caption">
+	<h4><a href="v-novom-sele/">Дом (коттедж), село Новое</a></h4>
+	<ul class="list-unstyled list-inline search-meters"><li>
+	<span class="stoit"><b>4 650 000</b> <i class="fa fa-rub"></i></span>
+	</li>
+	<li>181 м<sup>2</sup></li>
+	<li>8,6 соток</li>
+	</ul><p class="shosse tck">Суздальский район, село Новое</p>
+	</div>
+	</div>
+	</div>
+	<!-- /item -->
 
-<!-- item -->
-<div class="col-lg-4 col-md-4 col-sm-4">
-<div class="thumbnail">
-<a href="sudogda/"><img class="media-object img-responsive" alt="Дом (коттедж), село Новое" src="sudogda/sudogda.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 14</div>
-<div class="caption">
-<h4><a href="sudogda/">Дом (коттедж), Судогда</a></h4>
-<ul class="list-unstyled list-inline search-meters"><li>
-<span class="stoit"><b>2 600 000</b> <i class="fa fa-rub"></i></span>
-</li>
-<li>120 м<sup>2</sup></li>
-<li>12 соток</li>
-</ul><p class="shosse tck">Город Судогда, ул. Садовая</p>
-</div>
-</div>
-</div>
-<!-- /item -->
+	<!-- item -->
+	<div class="col-lg-4 col-md-4 col-sm-4">
+	<div class="thumbnail">
+	<a href="sudogda/"><img class="media-object img-responsive" alt="Дом (коттедж), село Новое" src="sudogda/sudogda.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 14</div>
+	<div class="caption">
+	<h4><a href="sudogda/">Дом (коттедж), Судогда</a></h4>
+	<ul class="list-unstyled list-inline search-meters"><li>
+	<span class="stoit"><b>2 600 000</b> <i class="fa fa-rub"></i></span>
+	</li>
+	<li>120 м<sup>2</sup></li>
+	<li>12 соток</li>
+	</ul><p class="shosse tck">Город Судогда, ул. Садовая</p>
+	</div>
+	</div>
+	</div>
+	<!-- /item -->
 
-<!-- item -->
-<div class="col-lg-4 col-md-4 col-sm-4">
-<div class="thumbnail">
-<a href="poselok-suvorotskoe/"><img class="media-object img-responsive" alt="Дом (коттедж), Суворотское" src="poselok-suvorotskoe/suvorotskoe.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 7</div>
-<div class="caption">
-<h4><a href="poselok-suvorotskoe/">Дом (коттедж), Суворотское</a></h4>
-<ul class="list-unstyled list-inline search-meters"><li>
-<span class="stoit"><b>3 000 000</b> <i class="fa fa-rub"></i></span>
-</li>
-<li>120 м<sup>2</sup></li>
-<li>8 соток</li>
-</ul><p class="shosse tck">Суздальский район, Суворотское</p>
-</div>
-</div>
-</div>
-<!-- /item -->
+	<!-- item -->
+	<div class="col-lg-4 col-md-4 col-sm-4">
+	<div class="thumbnail">
+	<a href="poselok-suvorotskoe/"><img class="media-object img-responsive" alt="Дом (коттедж), Суворотское" src="poselok-suvorotskoe/suvorotskoe.jpg"/></a><div class="photonum"><i class="fa fa-camera"></i> 7</div>
+	<div class="caption">
+	<h4><a href="poselok-suvorotskoe/">Дом (коттедж), Суворотское</a></h4>
+	<ul class="list-unstyled list-inline search-meters"><li>
+	<span class="stoit"><b>3 000 000</b> <i class="fa fa-rub"></i></span>
+	</li>
+	<li>120 м<sup>2</sup></li>
+	<li>8 соток</li>
+	</ul><p class="shosse tck">Суздальский район, Суворотское</p>
+	</div>
+	</div>
+	</div>
+	<!-- /item -->
 
+</div>
+
+<div class="row">
 <!-- item -->
 <div class="col-lg-4 col-md-4 col-sm-4">
 <div class="thumbnail">
@@ -115,6 +119,9 @@ include($root."blocks/header.php");
 </div>
 </div>
 <!-- /item -->
+</div>
+
+<div class="row">
 <!-- item -->
 <div class="col-lg-4 col-md-4 col-sm-4">
 <div class="thumbnail">
@@ -131,55 +138,12 @@ include($root."blocks/header.php");
 </div>
 </div>
 <!-- /item -->
-<div class="col-xs-12">
+</div>
+
 <p>Если вам необходимо купить дом во Владимирской области, то можем предложить загородные дома напрямую от застройщика.</p>
+
 </div>
 </div>
 
 <?php include($root."blocks/footer.php");?>
-<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-<script type="text/javascript">
-ymaps.ready(init);
 
-function init(){ 
-	$('#map').css({'width':'auto','height':'500px', 'margin-bottom':'20px'});    
-    var myMap;
-    myMap = new ymaps.Map("map", {
-        center: [56.185547,40.717593],
-        zoom: 9,
-        controls: []
-    });
-    myMap.controls.add("zoomControl", {
-        position: {top: 15, left: 15}
-    });
-	myCollection = new ymaps.GeoObjectCollection(null, {preset: 'islands#redIcon'}),
-	$('.thumbnail').each(function(i, elem) {
-		var arr = new Array($(elem).find('a').attr('href'),$(elem).find('img').attr('src'),$(elem).find('.tck').text(),$(elem).find('.caption li').text())
-		ymaps.geocode(arr[2], {results: 1}).then(function (res) {
-			var html = '<div class="popup">';
-				html +=	'<a href="'+arr[0]+'"><img src="'+arr[1]+'" alt="" width="100" /></a>';
-				html +=	'<div class="popup-text">';
-				html +=	'<span>Коттедж</span><br>';
-				html +=	'<span>'+arr[2]+'</span><br>';
-				html +=	'<span><b>'+arr[3]+'</span><br>';
-				html +=	'<a href="'+arr[0]+'">Подробнее</a>';
-				html +=	'</div></div>';
-
-			myCollection.add(new ymaps.Placemark(res.geoObjects.get(0).geometry.getCoordinates(),
-			{ balloonContent: html},
-			{ iconLayout: 'default#image',
-			  iconImageHref: '<?php echo HTTP_SERVER; ?>img/min_marker.png',
-			  iconImageSize: [40, 51],
-			  iconImageOffset: [-20, -47],
-			  
-			  balloonContentSize: [289, 151],
-			  balloonImageHref: '<?php echo HTTP_SERVER; ?>img/min_popup.png',
-			  balloonImageOffset: [-144, -147],
-			  balloonImageSize: [289, 151],
-			  balloonShadow: true }));
-		});
-	});
-	myMap.geoObjects.add(myCollection);
-//	myMap.setBounds(myCollection.getBounds());
-}
-</script>
