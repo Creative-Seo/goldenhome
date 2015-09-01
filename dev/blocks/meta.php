2b4e52a799c4416d41780830a210c7acc50469e7
ilcomp <cgde88@gmail.com>
2015-09-01 13:01:54 +0300

    u

--- dev/blocks/meta.php
+++ dev/blocks/meta.php
@@ -28,8 +28,10 @@
 <!-- build:remove -->
 	<link href="/css/style.css" rel="stylesheet">
 <!-- /build -->
+<!-- build:template
 	<script>
 	(function(u){function loadCSS(e,t,n){"use strict";function r(){for(var t,i=0;i<d.length;i++)d[i].href&&d[i].href.indexOf(e)>-1&&(t=!0);t?o.media=n||"all":setTimeout(r)}var o=window.document.createElement("link"),i=t||window.document.getElementsByTagName("script")[0],d=window.document.styleSheets;return o.rel="stylesheet",o.href=e,o.media="only x",i.parentNode.insertBefore(o,i),r(),o}for(var i in u){loadCSS(u[i]);}}(['/css/style.min.css', 'http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,cyrillic','http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=latin,cyrillic','http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&subset=latin,cyrillic','http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900&subset=latin,cyrillic']));
 	</script>
+<!-- /build -->
 </head>
 <body>
