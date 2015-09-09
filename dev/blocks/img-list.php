5c00511da692a5ec62d9182c2e749ab2572286b0
ilcomp <cgde88@gmail.com>
2015-09-09 12:20:16 +0300

    u

--- dev/blocks/img-list.php
+++ dev/blocks/img-list.php
@@ -1,5 +1,6 @@
 <div class="row">
 <?php 
+if (!($preview)) {$preview = 'img/';}
 $images = scandir('img/');
 if (false !== $images) {
     $imgarray = preg_grep('/\\.(?:jpe?g)$/', $images);
@@ -7,7 +8,7 @@ if (false !== $images) {
 ?>  
 	<div class="<?php echo $photo_col;?>">
 		<a href="<?php echo 'img/',htmlspecialchars(urlencode($row));?>" class="fancybox-thumb" rel="group" title="<?php echo $photo_title;?>">
-			<img class="img-responsive" src="<?php echo 'img/',htmlspecialchars(urlencode($row));?>" alt="<?php echo $photo_alt;?>">
+			<img class="img-responsive" src="<?php echo $preview,htmlspecialchars(urlencode($row));?>" alt="<?php echo $photo_alt;?>">
 	</a></div>
 <?php }} ?>
 </div>
